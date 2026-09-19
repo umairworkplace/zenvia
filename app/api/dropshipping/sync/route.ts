6f2cd2910c24e9058d85ac819eead354afc38dca
umairworkplace <insidehumanlab58@gmail.com>
2026-09-19 06:22:07 -0700

    feat: add manual supplier sync endpoint

---
+++ app/api/dropshipping/sync/route.ts
@@ -0,0 +1,2 @@
+import {NextResponse} from "next/server"; import {runSupplierSync} from "../../../../../lib/dropshipping/sync";
+export async function POST(){return NextResponse.json(await runSupplierSync())}
