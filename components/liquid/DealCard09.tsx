d7118ed39b58885bedfe79a386893589ba7dc933
umairworkplace <insidehumanlab58@gmail.com>
2026-09-19 05:48:23 -0700

    feat: add deal card 09

---
+++ components/liquid/DealCard09.tsx
@@ -0,0 +1,1 @@
+export function DealCard09({title,discount}:{title:string;discount:string}){return <article className="liquid-deal"><small>LIMITED DROP</small><h3>{title}</h3><strong>{discount}</strong></article>}
