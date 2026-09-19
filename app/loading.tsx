1a2e8948b61a00a49cebf51781fcdc3131817308
umairworkplace <insidehumanlab58@gmail.com>
2026-09-19 02:27:59 -0700

    feat: add route loading UI

---
+++ app/loading.tsx
@@ -0,0 +1,2 @@
+import {LoadingState} from "../components/LoadingState";
+export default function Loading(){return <main className="page"><LoadingState label="Preparing your shopping space"/></main>}
