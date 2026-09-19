6ea43224e93023ddaa4284bff52e15b898a2ce95
umairworkplace <insidehumanlab58@gmail.com>
2026-09-19 05:20:47 -0700

    design: add lifestyle glass promo

---
+++ components/GlassPromoCard.tsx
@@ -0,0 +1,2 @@
+import Link from "next/link";
+export function GlassPromoCard(){return <section className="glass-promo"><p>Upgrade Your<br/><b>Lifestyle</b></p><Link href="/shop">Shop now →</Link></section>}
