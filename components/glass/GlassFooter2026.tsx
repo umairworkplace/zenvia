4622c2c910085ce83a856df537d7f5cde0e7292b
umairworkplace <insidehumanlab58@gmail.com>
2026-09-19 05:40:13 -0700

    feat: add glass footer

---
+++ components/glass/GlassFooter2026.tsx
@@ -0,0 +1,2 @@
+import Link from "next/link";
+export function GlassFooter2026(){return <footer className="glass-footer"><div><b>ZENVIA</b><p>Commerce, reimagined.</p></div><nav><Link href="/help">Help</Link><Link href="/returns">Returns</Link><Link href="/privacy">Privacy</Link><Link href="/terms">Terms</Link></nav></footer>}
