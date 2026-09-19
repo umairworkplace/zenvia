d70e4c3d402960a26ab536d2ececfabe19495d33
umairworkplace <insidehumanlab58@gmail.com>
2026-09-19 05:32:20 -0700

    feat: add liquid glass navigation

---
+++ components/liquid/GlassNav.tsx
@@ -0,0 +1,2 @@
+import Link from "next/link";
+export function GlassNav(){return <nav className="glass-nav"><Link href="/">ZENVIA</Link><div><Link href="/shop">Shop</Link><Link href="/zenverse">Zenverse</Link><Link href="/orders">Orders</Link></div><Link href="/cart">Cart</Link></nav>}
