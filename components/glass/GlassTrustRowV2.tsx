3eb997f25711c54d4fbc278ae367bd854ebe9e22
umairworkplace <insidehumanlab58@gmail.com>
2026-09-19 05:43:17 -0700

    feat: add glass trust row

---
+++ components/glass/GlassTrustRowV2.tsx
@@ -0,0 +1,1 @@
+export function GlassTrustRowV2(){return <div className="glass-trust-row"><span>◇ Secure payments</span><span>↺ Easy returns</span><span>✦ Verified sellers</span><span>⚡ Fast support</span></div>}
