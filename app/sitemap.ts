0cf5892b52851984122bf671d21ef7923209715c
umairworkplace <insidehumanlab58@gmail.com>
2026-09-19 02:28:15 -0700

    chore: add sitemap metadata

---
+++ app/sitemap.ts
@@ -0,0 +1,2 @@
+import type {MetadataRoute} from "next";
+export default function sitemap():MetadataRoute.Sitemap{return [{url:"https://zenvia.example",lastModified:new Date()},{url:"https://zenvia.example/shop",lastModified:new Date()}]}
